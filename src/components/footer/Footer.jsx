6fe1981e9c2c1eb0a66d76c4e0f8d41e3960b133
Karim Abdelkareem <132012097+Karim0690@users.noreply.github.com>
2024-12-21 12:04:36 +0200

    error solve

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,7 +8,7 @@ import React from "react";
 
 export default async function Footer() {
   const t = await getTranslations("footer");
-  const locale = cookies().get("locale").value;
+  const locale = cookies().get("locale")?.value;
 
   return (
     <div>
