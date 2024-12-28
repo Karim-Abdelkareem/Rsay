d1c7fe76044549b08dd312c59c3d027bcb31515e
Karim Abdelkareem <132012097+Karim0690@users.noreply.github.com>
2024-12-28 19:21:10 +0200

    fix RSAY

--- src/components/ui/typewriter-effect.jsx
+++ src/components/ui/typewriter-effect.jsx
@@ -5,7 +5,6 @@ import { motion, stagger, useAnimate, useInView } from "framer-motion";
 import { useEffect } from "react";
 
 export const TypewriterEffect = ({ words, className, cursorClassName }) => {
-  // Split text inside of words into array of characters
   const wordsArray = words.map((word) => {
     return {
       ...word,
@@ -38,17 +37,20 @@ export const TypewriterEffect = ({ words, className, cursorClassName }) => {
     return (
       <motion.div ref={scope} className="inline">
         {wordsArray.map((word, idx) => {
+          const isRTL = /[\u0600-\u06FF]/.test(word.text.join("")); // Detect Arabic characters
           return (
-            <div key={`word-${idx}`} className="inline-block">
+            <div
+              key={`word-${idx}`}
+              className={cn("inline-block", isRTL ? "rtl-safari" : "")}
+              dir={isRTL ? "rtl" : "ltr"} // Set direction dynamically
+            >
               {word.text.map((char, index) => (
                 <motion.span
                   initial={{}}
                   key={`char-${index}`}
                   className={cn(
                     `dark:text-white text-black opacity-0 hidden`,
-                    word.className,
-                    word.isRTL ? "whitespace-pre" : "", // Add specific class for Arabic
-                    word.isRTL ? "dir-rtl" : "" // Add RTL direction for Arabic text
+                    word.className
                   )}
                 >
                   {char}
@@ -83,92 +85,6 @@ export const TypewriterEffect = ({ words, className, cursorClassName }) => {
         }}
         className={cn(
           "inline-block rounded-sm w-[4px] h-4 md:h-6 lg:h-10 bg-blue-500",
-          cursorClassName
-        )}
-      ></motion.span>
-    </div>
-  );
-};
-
-export const TypewriterEffectSmooth = ({
-  words,
-  className,
-  cursorClassName,
-}) => {
-  // Split text inside of words into array of characters
-  const wordsArray = words.map((word) => {
-    return {
-      ...word,
-      text: word.text.split(""),
-    };
-  });
-
-  const renderWords = () => {
-    return (
-      <div>
-        {wordsArray.map((word, idx) => {
-          return (
-            <div key={`word-${idx}`} className="inline-block">
-              {word.text.map((char, index) => (
-                <span
-                  key={`char-${index}`}
-                  className={cn(
-                    `dark:text-white text-white`,
-                    word.isRTL ? "whitespace-pre" : "", // Add specific class for Arabic
-                    word.isRTL ? "dir-rtl" : "" // Add RTL direction for Arabic text
-                  )}
-                >
-                  {char}
-                </span>
-              ))}
-              &nbsp;
-            </div>
-          );
-        })}
-      </div>
-    );
-  };
-
-  return (
-    <div className={cn("flex space-x-1 my-6", className)}>
-      <motion.div
-        className="overflow-hidden "
-        initial={{
-          width: "0%",
-        }}
-        whileInView={{
-          width: "fit-content",
-        }}
-        transition={{
-          duration: 1,
-          ease: "linear",
-          delay: 1,
-        }}
-      >
-        <div
-          className="text-4xl md:text-8xl lg:text:8xl xl:text-8xl font-bold"
-          style={{
-            whiteSpace: "nowrap",
-          }}
-        >
-          {renderWords()}{" "}
-        </div>{" "}
-      </motion.div>
-      <motion.span
-        initial={{
-          opacity: 0,
-        }}
-        animate={{
-          opacity: 1,
-        }}
-        transition={{
-          duration: 0.8,
-
-          repeat: Infinity,
-          repeatType: "reverse",
-        }}
-        className={cn(
-          "block rounded-sm w-[4px]  h-12 md:h-28 bg-blue-500",
           cursorClassName
         )}
       ></motion.span>
